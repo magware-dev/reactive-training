b72342dc4b6d2916e8f8da70c8db5f5cc2f16ad9
Manuel Andreo Garcia <andreo.garcia@posteo.de>
2020-12-15 17:53:27 +0100

    added solution of lecture02

--- src/main/java/dev/magware/reactivetraining/openlibrary/OpenLibraryAPI.java
+++ src/main/java/dev/magware/reactivetraining/openlibrary/OpenLibraryAPI.java
@@ -37,8 +37,29 @@ public class OpenLibraryAPI {
     }
 
     public Mono<ISBNDetails> getDetailsByIsbn(String isbn) {
-        // TODO
-        return null;
+        return this.webClient
+            .get()
+            .uri(
+                uriBuilder -> uriBuilder
+                    .path(isbnApi)
+                    .build(isbn)
+            )
+            .exchange()
+            .flatMap(
+                clientResponse -> {
+                    switch (clientResponse.statusCode()) {
+                        case NOT_FOUND:
+                            return clientResponse.releaseBody()
+                                .then(Mono.empty());
+                        case OK:
+                            return clientResponse
+                                .bodyToMono(ISBNDetails.class);
+                        default:
+                            return clientResponse.releaseBody()
+                                .then(Mono.error(new RuntimeException(clientResponse.statusCode().toString())));
+                    }
+                }
+            );
     }
 
     @Data

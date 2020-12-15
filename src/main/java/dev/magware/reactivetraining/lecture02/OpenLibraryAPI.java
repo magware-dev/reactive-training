de4843917230a0d17efa978d0f5d31b97082d516
Manuel Andreo Garcia <andreo.garcia@posteo.de>
2020-12-15 18:16:08 +0100

    fixed formatting

--- src/main/java/dev/magware/reactivetraining/lecture02/OpenLibraryAPI.java
+++ src/main/java/dev/magware/reactivetraining/lecture02/OpenLibraryAPI.java
@@ -39,27 +39,27 @@ public class OpenLibraryAPI {
     public Mono<ISBNDetails> getDetailsByIsbn(String isbn) {
         return this.webClient
             .get()
-            .uri(
-                uriBuilder -> uriBuilder
-                    .path(isbnApi)
-                    .build(isbn)
-            )
+                .uri(
+                    uriBuilder -> uriBuilder
+                        .path(isbnApi)
+                        .build(isbn)
+                )
             .exchange()
-            .flatMap(
-                clientResponse -> {
-                    switch (clientResponse.statusCode()) {
-                        case NOT_FOUND:
-                            return clientResponse.releaseBody()
-                                .then(Mono.empty());
-                        case OK:
-                            return clientResponse
-                                .bodyToMono(ISBNDetails.class);
-                        default:
-                            return clientResponse.releaseBody()
-                                .then(Mono.error(new RuntimeException(clientResponse.statusCode().toString())));
+                .flatMap(
+                    clientResponse -> {
+                        switch (clientResponse.statusCode()) {
+                            case NOT_FOUND:
+                                return clientResponse.releaseBody()
+                                    .then(Mono.empty());
+                            case OK:
+                                return clientResponse
+                                    .bodyToMono(ISBNDetails.class);
+                            default:
+                                return clientResponse.releaseBody()
+                                    .then(Mono.error(new RuntimeException(clientResponse.statusCode().toString())));
+                        }
                     }
-                }
-            );
+                );
     }
 
     @Data
